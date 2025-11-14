fb9e56826e8aa1107fb8837d01c50055ad983aab
optout <13562139+optout21@users.noreply.github.com>
2025-11-14 21:03:11 +0100

    Add nostr.json

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,4 +1,13 @@
+# build files
 npm run docs:build
-cp -R .vitepress/dist/* /var/www/zappool.org
+
+# copy files
+ROOT=/var/www/zappool.org
+cp -R .vitepress/dist/* $ROOT
+
+# copy nostr.json
+mkdir -p $ROOT/.well-known
+cp well-known/nostr.json $ROOT/.well-known
+
 ls -alt /var/www/zappool.org
 systemctl restart nginx
